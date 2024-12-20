c84e4dfb129c9686494fab4f637ab1f2648be464
jonofre <jonofre0805@gmail.com>
2024-12-19 21:26:12 -0600

    Initial check-in

---
+++ ResumeAPI/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResumeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet(Name = "GetSkillsList")]
+        public IEnumerable<string> Get()
+        {
+            List<string> skills = new List<string>(){ "SQL", "C#", "ASP.NET Core" };
+            return skills.ToArray();
+        }
+    }
+}
