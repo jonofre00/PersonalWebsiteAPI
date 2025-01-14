6b8acf1a5062386d3d6b5805effffff8b1ac2344
jonofre <jonofre0805@gmail.com>
2025-01-13 21:01:14 -0600

    Installed Netlify package.

--- ResumeAPI/Program.cs
+++ ResumeAPI/Program.cs
@@ -1,4 +1,7 @@
+using NetlifySharp;
+
 var builder = WebApplication.CreateBuilder(args);
+var client = new NetlifyClient("nfp_MpQQvXLqNinQBGmH24LW745JKadgL6Q7b48f");
 
 // Add services to the container.
 
